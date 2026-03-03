343c2fa2bf8c1661d988d61ac2de65426a4212c6
luisbebber <luis.fbebber@gmail.com>
2026-03-03 19:49:13 -0300

    :hammer: update: get course id and post course routes updated

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import fastify from 'fastify'
-// import crypto from 'node:crypto'
+import { eq } from 'drizzle-orm'
 import { db } from './src/database/client.ts'
 import { courses } from './src/database/schema.ts'
 
@@ -15,49 +15,54 @@ const server = fastify({
   },
 })
 
-// lista velha de cursos
-
 server.get('/courses', async (request, reply) => {
-  const result = await db.select().from(courses)
+  const result = await db.select({
+    id: courses.id,
+    title: courses.title,
+  }).from(courses)
 
   return reply.send({ courses: result })
 })
 
-// server.get('/courses/:id', (request, reply) => {
-//   type Params = {
-//     id: string
-//   }
+server.get('/courses/:id', async (request, reply) => {
+  type Params = {
+    id: string
+  }
 
-//   const params = request.params as Params
-//   const courseId = params.id
+  const params = request.params as Params
+  const courseId = params.id
 
-//   const course = courses.find(course => course.id === courseId)
+  const result = await db
+    .select()
+    .from(courses)
+    .where(eq(courses.id, courseId))
 
-//   if (course) {
-//     return { course }
-//   }
+  if (result.length > 0) {
+    return { course: result[0] }
+  }
 
-//   return reply.status(404).send()
-// })
+  return reply.status(404).send()
+})
  
-// server.post('/courses', (request, reply) => {
-//   type Body = {
-//     title: string
-//   }
+server.post('/courses', async (request, reply) => {
+  type Body = {
+    title: string
+  }
 
-//   const courseId = crypto.randomUUID()
+  const body = request.body as Body
+  const courseTitle = body.title
 
-//   const body = request.body as Body
-//   const courseTitle = body.title
+  if (!courseTitle) {
+    return reply.status(400).send({ message: 'Título obrigatório.' })
+  }
 
-//   if (!courseTitle) {
-//     return reply.status(400).send({ message: 'Título obrigatório.' })
-//   }
+  const result = await db
+    .insert(courses)
+    .values({ title: courseTitle })
+    .returning()
 
-//   courses.push({ id: courseId, title: courseTitle })
-
-//   return reply.status(201).send({ courseId })
-// })
+  return reply.status(201).send({ courseId: result[0].id })
+})
 
 server.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
